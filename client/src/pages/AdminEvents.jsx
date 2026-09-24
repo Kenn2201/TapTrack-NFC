92c40a24b9e017a26445116cc24515d144516c57
Kenn Nacario <114212602+Kenn2201@users.noreply.github.com>
2026-09-24 17:28:42 +0800

    refactor: use shared route shell header

--- client/src/pages/AdminEvents.jsx
+++ client/src/pages/AdminEvents.jsx
@@ -1,5 +1,4 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import Header from '../components/layout/Header';
 import AdminWorkspaceNav from '../components/layout/AdminWorkspaceNav';
 import PageContainer from '../components/ui/PageContainer';
 import PageHeader from '../components/ui/PageHeader';
@@ -195,7 +194,6 @@ export default function AdminEvents() {
 
   return (
     <div className="min-h-screen bg-slate-950 text-slate-100 flex flex-col">
-      <Header />
       <AdminWorkspaceNav />
 
       <PageContainer maxWidth="max-w-7xl">
