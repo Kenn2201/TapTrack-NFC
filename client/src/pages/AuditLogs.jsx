830bb246b21c197d900ba2312f80c2770f207540
Kenn Nacario <114212602+Kenn2201@users.noreply.github.com>
2026-09-24 17:29:02 +0800

    refactor: use shared route shell header

--- client/src/pages/AuditLogs.jsx
+++ client/src/pages/AuditLogs.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useMemo, useState } from 'react';
-import Header from '../components/layout/Header';
 import AdminWorkspaceNav from '../components/layout/AdminWorkspaceNav';
 import PageContainer from '../components/ui/PageContainer';
 import PageHeader from '../components/ui/PageHeader';
@@ -187,7 +186,6 @@ export default function AuditLogs() {
 
   return (
     <div className="min-h-screen bg-slate-950 text-white flex flex-col">
-      <Header />
       <AdminWorkspaceNav />
 
       <PageContainer maxWidth="max-w-6xl">
